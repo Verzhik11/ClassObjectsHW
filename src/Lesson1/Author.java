840ad2d1b69cef6b64d785b4a4c01de382c335a7
Verzhik <verzhik11@gmail.com>
2023-02-08 21:38:20 +0300

    Домашка по классам и объектам. Часть 2 выполнена.

--- src/Lesson1/Author.java
+++ src/Lesson1/Author.java
@@ -1,5 +1,7 @@
 package Lesson1;
 
+import java.util.Objects;
+
 public class Author {
     private String name;
     private String surname;
@@ -14,6 +16,30 @@ public class Author {
 
     public String getSurname() {
         return this.surname;
+    }
+    public String toString () {
+        return "Имя автора " + this.name + " Фамилия автора " + this.surname;
+    }
+    //public boolean equals(Object other) {
+        //if (this.getClass() != other.getClass()) {
+            //return false;
+        //}
+        //Author authorLOR = (Author) other;
+        //return surname.equals(authorLOR.surname);
+
+    //}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Author author = (Author) o;
+        return Objects.equals(name, author.name) && Objects.equals(surname, author.surname);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surname);
     }
 }
 
